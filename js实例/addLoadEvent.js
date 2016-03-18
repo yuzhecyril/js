6cbc6dde698cc056358a7e5ff9ac38b589176162
yuzhecyril <58627312@qq.com>
2016-03-18 18:59:50 +0800

    addLoadEvent

---
+++ js实例/addLoadEvent.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yuzhe on 2016/3/18.
+ *替代window.onload
+ */
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        }
+    }
+}
